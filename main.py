645c730ce8f26fe5f9ecc3b6a65f0e84267aa83f
acSirius <angelcerveraroldan765@gmail.com>
2020-07-22 15:02:46 +0100

    The app now checks if the wikipedia article exists

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import requests
 import os
 import json 
-import requests
 import random 
 import string
 import webbrowser
+from bs4 import BeautifulSoup
 
 words = json.loads(open('common_words.json').read())
 
@@ -12,9 +13,17 @@ def random_word():
 
 word = random_word()
 URL = f'https://en.wikipedia.org/wiki/{word}'
+#URL = 'https://en.wikipedia.org/wiki/lsafdj1234'
 
-webbrowser.open(URL)
+r = requests.get(URL)
+soup = BeautifulSoup(r.content, 'html.parser')
 
+not_found_sintaxs = soup.findAll('b')
+print(URL)
 
-
-
+if not_found_sintaxs[1].get_text() != 'Wikipedia does not have an article with this exact name.':
+    word = random_word()
+    URL = f'https://en.wikipedia.org/wiki/{word}'
+    webbrowser.open(URL)
+else:
+    webbrowser.open(URL)
